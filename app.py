29f38fd8d9c1ec60791bc69bd164f8b5cc7247a3
Sushant Patil <160316172+sush-sp777@users.noreply.github.com>
2025-11-30 20:31:39 +0530

    Fix syntax for accessing secrets in app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-os.environ["LANGCHAIN_API_KEY"]=st.secrets("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_API_KEY"]=st.secrets["LANGCHAIN_API_KEY"]
 os.environ["LANGCHAIN_TRACING_V2"]="true"
 os.environ["LANGCHAIN_PROJECT"]="Q&A Chatbot -Groq"
 
